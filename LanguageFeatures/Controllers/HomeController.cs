444d6b59a3c9b2f23f93ec3fd1c93fdfb8f8cc15
Muso <anon8434337@PM.me>
2025-06-05 14:20:42 -0400

    Add MVC support.

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿namespace LanguageFeatures.Controllers 
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ShoppingCart cart = new ShoppingCart
+            {
+                Products = Product.GetProducts()
+            };
+            Product[] productArray =
+            {
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name = "Lifejacket", Price = 48.95M },
+            };
+            decimal cartTotal = cart.TotalPrices();
+            decimal arrayTotal = productArray.TotalPrices();
+            return View("Index", new string[] { $"Cart Total: {cartTotal:C2}", $"Array Total: {arrayTotal:C2}"});
+        }
+    }
+}
