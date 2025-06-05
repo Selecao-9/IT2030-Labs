a99ca38a4c1e16f9ba2e092546e3faef8c97be50
Muso <anon8434337@PM.me>
2025-06-05 15:12:16 -0400

    Add more models.

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 {
     public class HomeController : Controller
     {
+        //bool FilterByPrice(Product? p) { return (p?.Price ?? 0) >= 20 };
         public ViewResult Index()
         {
             ShoppingCart cart = new ShoppingCart
@@ -12,10 +13,15 @@
             {
                 new Product { Name = "Kayak", Price = 275M },
                 new Product { Name = "Lifejacket", Price = 48.95M },
+                new Product { Name = "Soccer Ball", Price = 19.50M },
+                new Product { Name = "Corner Flag", Price = 34.95M }
             };
-            decimal cartTotal = cart.TotalPrices();
-            decimal arrayTotal = productArray.TotalPrices();
-            return View("Index", new string[] { $"Cart Total: {cartTotal:C2}", $"Array Total: {arrayTotal:C2}"});
+
+            //Func<Product?, bool> nameFilter = delegate (Product? prod) { return prod?.Name?[0] == 'S'; };
+
+            decimal priceFilterTotal = productArray.Filter(p => (p?.Price ?? 0) >= 20 ).TotalPrices();
+            decimal nameFilterTotal = productArray.Filter(p => p?.Name[0] == 'S').TotalPrices();
+            return View("Index", new string[] { $"Price Total: {priceFilterTotal:C2}", $"Name Total: {nameFilterTotal:C2}" });
         }
     }
 }
