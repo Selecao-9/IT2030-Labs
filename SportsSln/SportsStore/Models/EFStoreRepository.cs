ca4f20f851cdd72aae1668ebb2479ee6c38a7aea
Muso <anon8434337@PM.me>
2025-06-13 13:54:40 -0400

    Add support for a database.

---
+++ SportsSln/SportsStore/Models/EFStoreRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace SportsStore.Models
+{
+    public class EFStoreRepository : IStoreRepository
+    {
+        private StoreDbContext context;
+        public EFStoreRepository(StoreDbContext ctx)
+        {
+            context = ctx;
+        }
+        public IQueryable<Product> Products => context.Products;
+        // Additional methods for adding, updating, and deleting products can be added here
+    }
+}
