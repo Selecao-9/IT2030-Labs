849c4ba8516700828e82435d2d45ead1593452f7
Muso <anon8434337@PM.me>
2025-06-13 14:44:07 -0400

    Add a HomeControllerTests file.

---
+++ SportsSln/SportsStore/Models/SeedData.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            StoreDbContext context = app.ApplicationServices
+                .CreateScope().ServiceProvider
+                .GetRequiredService<StoreDbContext>();
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Kayak",
+                        Description = "A boat for one person",
+                        Price = 275M,
+                        Category = "Watercraft"
+                    },
+                    new Product
+                    {
+                        Name = "Lifejacket",
+                        Description = "Protective and fashionable",
+                        Price = 48.95M,
+                        Category = "Watercraft"
+                    },
+                    new Product
+                    {
+                        Name = "Soccer Ball",
+                        Description = "FIFA-approved size and weight",
+                        Price = 19.50M,
+                        Category = "Soccer"
+                    },
+                    new Product
+                    {
+                        Name = "Corner Flags",
+                        Description = "Give your playing field a professional touch",
+                        Price = 34.95M,
+                        Category = "Soccer"
+                    },
+                    new Product
+                    {
+                        Name = "Stadium",
+                        Description = "Flat-packed 35,000-seat stadium",
+                        Price = 79500M,
+                        Category = "Soccer"
+                    },
+                    new Product
+                    {
+                        Name = "Thinking Cap",
+                        Description = "Improve your brain efficiency by 75%",
+                        Price = 16M,
+                        Category = "Chess"
+                    },
+                    new Product
+                    {
+                        Name = "Unsteady Chair",
+                        Description = "Secretly give your opponent a disadvantage",
+                        Price = 29.95M,
+                        Category = "Chess"
+                    },
+                    new Product
+                    {
+                        Name = "Human Chess Board",
+                        Description = "A fun game for the family",
+                        Price = 75M,
+                        Category = "Chess"
+                    },
+                    new Product
+                    {
+                        Name = "Bling-Bling King",
+                        Description = "Gold-plated, diamond-studded King",
+                        Category = "Chess", Price = 1200M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- SportsSln/SportsStore/Program.cs
+++ SportsSln/SportsStore/Program.cs
@@ -20,4 +20,6 @@ var app = builder.Build();
 app.UseStaticFiles();
 app.MapDefaultControllerRoute();
 
+SeedData.EnsurePopulated(app);
+
 app.Run();
