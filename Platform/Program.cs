f1fc855e9ffbaa265398c2981719ed898a700b1e
Muso <anon8434337@PM.me>
2025-06-17 14:22:19 -0400

    Further testing.

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -1,16 +1,26 @@
-//using Microsoft.Extensions.Options;
 using Platform;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<MessageOptions>(options => {
-    options.CityName = "Albany";
-});
-
 var app = builder.Build();
 
-app.UseMiddleware<LocationMiddleware>();
+//app.UseMiddleware<Population>();
+//app.UseMiddleware<Capital>();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+#pragma warning disable ASP0014
+
+app.UseEndpoints(endpoints => {
+    endpoints.MapGet("routing", async context => {
+        await context.Response.WriteAsync("Request Was Routed");
+    });
+    endpoints.MapGet("capital/uk", new Capital().Invoke);
+    endpoints.MapGet("population/paris", new Population().Invoke);
+});
+
+app.Run(async (context) => {
+    await context.Response.WriteAsync("Terminal Middleware Reached");
+});
 
 app.Run();
