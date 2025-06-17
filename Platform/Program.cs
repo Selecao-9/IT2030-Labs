f7a165be55a83973478db847f9b0ff46b30ed759
Muso <anon8434337@PM.me>
2025-06-17 16:37:38 -0400

    Testing custom constraints.

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -2,16 +2,14 @@ using Platform;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<RouteOptions>(opts => {
+    opts.ConstraintMap.Add("countryName",
+        typeof(CountryRouteConstraint));
+});
+
 var app = builder.Build();
 
-app.MapGet("{first:alpha:length(3)}/{second:bool}", async context => {
-    await context.Response.WriteAsync("Request Was Routed\n");
-    foreach (var kvp in context.Request.RouteValues)
-    {
-        await context.Response
-            .WriteAsync($"{kvp.Key}: {kvp.Value}\n");
-    }
-});
+app.MapGet("capital/{country:countryName}", Capital.Endpoint);
 
 app.MapGet("capital/{country:regex(^uk|france|monaco$)}",
     Capital.Endpoint);
