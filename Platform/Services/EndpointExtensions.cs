2d767220800e95ee181bd398af35549d4d0588b2
Muso <anon8434337@PM.me>
2025-06-18 16:45:07 -0400

    Further testing scoped services.

--- Platform/Services/EndpointExtensions.cs
+++ Platform/Services/EndpointExtensions.cs
@@ -15,17 +15,22 @@ namespace Microsoft.AspNetCore.Builder
             {
                 throw new System.Exception("Method cannot be used");
             }
-            T endpointInstance =
-                ActivatorUtilities.CreateInstance<T>(app.ServiceProvider);
+            //T endpointInstance =
+            //ActivatorUtilities.CreateInstance<T> (app.ServiceProvider);
 
             ParameterInfo[] methodParams = methodInfo!.GetParameters();
 
-            app.MapGet(path, context =>
-                (Task)methodInfo.Invoke(endpointInstance,
-                  methodParams.Select(p =>
-                    p.ParameterType == typeof(HttpContext) ? context
-                      : app.ServiceProvider.GetService(p.ParameterType))
-                    .ToArray())!);
+            app.MapGet(path, context => {
+                T endpointInstance =
+                    ActivatorUtilities.CreateInstance<T>
+                        (context.RequestServices);
+                return (Task)methodInfo.Invoke(endpointInstance!,
+                    methodParams.Select(p =>
+                    p.ParameterType == typeof(HttpContext)
+                    ? context
+                    : context.RequestServices.GetService(p.ParameterType))
+                        .ToArray())!;
+            });
         }
     }
 }
