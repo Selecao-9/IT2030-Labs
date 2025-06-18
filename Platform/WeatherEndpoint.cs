cba3968d6db85e003a8418a63411125cc489e503
Muso <anon8434337@PM.me>
2025-06-18 14:48:59 -0400

    Testing the singleton pattern.

---
+++ Platform/WeatherEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using Platform.Services;
+
+namespace Platform
+{
+    public class WeatherEndpoint
+    {
+
+        public static async Task Endpoint(HttpContext context)
+        {
+            await TextResponseFormatter.Singleton.Format(context,
+                "Endpoint Class: It is cloudy in Milan");
+        }
+    }
+}
+
