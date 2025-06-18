4ba4864b5f6572075795652811550628596f29ab
Muso <anon8434337@PM.me>
2025-06-18 15:36:41 -0400

    Testing dependency injection.

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -3,16 +3,20 @@ using Platform.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<IResponseFormatter,
+    HtmlResponseFormatter>();
+
 var app = builder.Build();
 
 app.UseMiddleware<WeatherMiddleware>();
 
 app.MapGet("endpoint/class", WeatherEndpoint.Endpoint);
 
-IResponseFormatter formatter = TypeBroker.Formatter;
-app.MapGet("endpoint/function", async context => {
-    await formatter.Format(context,
-        "Endpoint Function: It is sunny in LA");
-});
+//IResponseFormatter formatter = TypeBroker.Formatter;
+app.MapGet("endpoint/function",
+    async (HttpContext context, IResponseFormatter formatter) => {
+        await formatter.Format(context,
+            "Endpoint Function: It is sunny in LA");
+    });
 
 app.Run();
--- Platform/WeatherEndpoint.cs
+++ Platform/WeatherEndpoint.cs
@@ -1,12 +1,16 @@
 ﻿using Platform.Services;
 
-namespace Platform {
-    public class WeatherEndpoint {
-        
-        public static async Task Endpoint(HttpContext context) {
-            await TypeBroker.Formatter.Format(context, 
+namespace Platform
+{
+    public class WeatherEndpoint
+    {
+
+        public static async Task Endpoint(HttpContext context)
+        {
+            IResponseFormatter formatter = context.RequestServices
+                .GetRequiredService<IResponseFormatter>();
+            await formatter.Format(context,
                 "Endpoint Class: It is cloudy in Milan");
         }
     }
 }
-
