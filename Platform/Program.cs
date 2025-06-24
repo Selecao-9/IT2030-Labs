1e1995698fa48a184bd38a2841963c4443f3ca41
Muso <anon8434337@PM.me>
2025-06-24 15:12:26 -0400

    Added some configuration.

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -1,6 +1,24 @@
+using Platform;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var servicesConfig = builder.Configuration;
+builder.Services.Configure<MessageOptions>(
+    servicesConfig.GetSection("Location"));
+
 var app = builder.Build();
+
+var pipelineConfig = app.Configuration;
+// - use configuration settings to set up pipeline
+
+app.UseMiddleware<LocationMiddleware>();
+
+app.MapGet("config", async (HttpContext context,
+        IConfiguration config) => {
+            string? defaultDebug = config["Logging:LogLevel:Default"];
+            await context.Response
+                .WriteAsync($"The config setting is: {defaultDebug}");
+        });
 
 app.MapGet("/", async context => {
     await context.Response.WriteAsync("Hello World!");
