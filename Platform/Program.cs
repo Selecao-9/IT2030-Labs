d1f2dfc5d2bfb08018ccaf0ea5e9bcc44473219a
Muso <anon8434337@PM.me>
2025-06-24 16:18:44 -0400

    Added further test functionality of debugging messages.

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -4,6 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+app.Logger.LogDebug("Pipeline configuration starting");
+
 app.MapGet("population/{city?}", Population.Endpoint);
+
+app.Logger.LogDebug("Pipeline configuration complete");
 
 app.Run();
