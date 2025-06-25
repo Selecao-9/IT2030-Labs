b50813f2b5135ec915f981a15acf6b6e7ae185f4
Muso <anon8434337@PM.me>
2025-06-25 16:21:01 -0400

    Testing HttpLogging middleware.

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -4,13 +4,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-var logger = app.Services
-       .GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
+app.UseHttpLogging();
 
-logger.LogDebug("Pipeline configuration starting");
+//var logger = app.Services
+//  .GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
+
+//logger.LogDebug("Pipeline configuration starting");
 
 app.MapGet("population/{city?}", Population.Endpoint);
 
-logger.LogDebug("Pipeline configuration complete");
+//logger.LogDebug("Pipeline configuration complete");
 
 app.Run();
