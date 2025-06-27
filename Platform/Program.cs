4a3150d2f27aecf5c77368d8775212f65823ba22
Muso <anon8434337@PM.me>
2025-06-27 14:04:19 -0400

    Test custom error landing page.

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -2,6 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error.html");
+    app.UseStaticFiles();
+}
+
 app.Run(context => {
     throw new Exception("Something has gone wrong");
 });
