87375634d38e9d2885d58a204a597784f71c0116
Muso <anon8434337@PM.me>
2025-06-27 13:57:08 -0400

    Reset program.cs to test exception handling.

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -1,54 +1,9 @@
-using Microsoft.AspNetCore.RateLimiting;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddDistributedMemoryCache();
-
-builder.Services.AddSession(opts => {
-    opts.IdleTimeout = TimeSpan.FromMinutes(30);
-    opts.Cookie.IsEssential = true;
-});
-
-builder.Services.AddHsts(opts => {
-    opts.MaxAge = TimeSpan.FromDays(1);
-    opts.IncludeSubDomains = true;
-});
-
-builder.Services.AddRateLimiter(opts => {
-    opts.AddFixedWindowLimiter("fixedWindow", fixOpts => {
-        fixOpts.PermitLimit = 1;
-        fixOpts.QueueLimit = 0;
-        fixOpts.Window = TimeSpan.FromSeconds(15);
-    });
-});
 
 var app = builder.Build();
 
-if (app.Environment.IsProduction())
-{
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseRateLimiter();
-
-app.UseSession();
-
-app.MapGet("/session", async context => {
-    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
-    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
-    context.Session.SetInt32("counter1", counter1);
-    context.Session.SetInt32("counter2", counter2);
-    await context.Session.CommitAsync();
-    await context.Response
-        .WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
-}).RequireRateLimiting("fixedWindow");
-
-app.MapFallback(async context => {
-    await context.Response
-        .WriteAsync($"HTTPS Request: {context.Request.IsHttps} \n");
-    await context.Response.WriteAsync("Hello World!");
+app.Run(context => {
+    throw new Exception("Something has gone wrong");
 });
 
 app.Run();
