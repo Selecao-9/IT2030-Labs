ecbcb422e1b6cf684fd29f8a488fb6d7c898fe25
Muso <anon8434337@PM.me>
2025-06-27 14:18:36 -0400

    Testing status code enrichment.

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -8,8 +8,23 @@ if (!app.Environment.IsDevelopment())
     app.UseStaticFiles();
 }
 
+app.UseStatusCodePages("text/html", Platform.Responses.DefaultResponse);
+
+app.Use(async (context, next) => {
+    if (context.Request.Path == "/error")
+    {
+        context.Response.StatusCode = StatusCodes.Status404NotFound;
+        await Task.CompletedTask;
+    }
+    else
+    {
+        await next();
+    }
+});
+
 app.Run(context => {
     throw new Exception("Something has gone wrong");
 });
 
 app.Run();
+    
