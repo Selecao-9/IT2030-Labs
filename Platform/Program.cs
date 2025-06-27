92fdc0271d5f192071fe68eb986a62448e0c0bb3
Muso <anon8434337@PM.me>
2025-06-27 12:40:54 -0400

    Testing https.

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -1,42 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<CookiePolicyOptions>(opts => {
-    opts.CheckConsentNeeded = context => true;
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(opts => {
+    opts.IdleTimeout = TimeSpan.FromMinutes(30);
+    opts.Cookie.IsEssential = true;
 });
 
 var app = builder.Build();
 
-app.UseCookiePolicy();  
-app.UseMiddleware<Platform.ConsentMiddleware>();
+app.UseSession();
 
-app.MapGet("/cookie", async context => {
-    int counter1 =
-        int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
-    context.Response.Cookies.Append("counter1", counter1.ToString(),
-        new CookieOptions
-        {
-            MaxAge = TimeSpan.FromMinutes(30),
-            IsEssential = true
-        });
-    int counter2 =
-        int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
-    context.Response.Cookies.Append("counter2", counter2.ToString(),
-        new CookieOptions
-        {
-            MaxAge = TimeSpan.FromMinutes(30)
-        });
+app.MapGet("/session", async context => {
+    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
+    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
+    context.Session.SetInt32("counter1", counter1);
+    context.Session.SetInt32("counter2", counter2);
+    await context.Session.CommitAsync();
     await context.Response
         .WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
 });
 
-app.MapGet("clear", context => {
-    context.Response.Cookies.Delete("counter1");
-    context.Response.Cookies.Delete("counter2");
-    context.Response.Redirect("/");
-    return Task.CompletedTask;
+app.MapFallback(async context => {
+    await context.Response
+        .WriteAsync($"HTTPS Request: {context.Request.IsHttps} \n");
+    await context.Response.WriteAsync("Hello World!");
 });
 
-app.MapFallback(async context =>
-    await context.Response.WriteAsync("Hello World!"));
 
 app.Run();
