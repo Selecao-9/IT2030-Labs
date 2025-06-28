f938e01aed725646ba4a04476bf73e66d370db2e
Muso <anon8434337@PM.me>
2025-06-28 16:55:44 -0400

    Testing caching policies.

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -9,7 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 //    opts.TableName = "DataCache";
 //});
 
-builder.Services.AddOutputCache();
+builder.Services.AddOutputCache(opts => {
+    opts.AddBasePolicy(policy => {
+        policy.Cache();
+        policy.Expire(TimeSpan.FromSeconds(10));
+    });
+    opts.AddPolicy("30sec", policy => {
+        policy.Cache(); 
+        policy.Expire(TimeSpan.FromSeconds(30));
+    });
+});
 
 //builder.Services.AddResponseCaching();
 builder.Services.AddSingleton<IResponseFormatter,
@@ -23,6 +32,10 @@ app.UseOutputCache();
 app.MapEndpoint<Platform
     .SumEndpoint>("/sum/{count:int=1000000000}")
     .CacheOutput();
+
+app.MapEndpoint<Platform
+    .SumEndpoint>("/sum30/{count:int=1000000000}")
+    .CacheOutput("30sec");
 
 app.MapGet("/", async context => {
     await context.Response.WriteAsync("Hello World!");
