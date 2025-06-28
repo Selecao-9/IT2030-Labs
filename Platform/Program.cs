423d6814729c0bbc794b1774fa8bc8b4e7f4ccb7
Muso <anon8434337@PM.me>
2025-06-28 11:25:06 -0400

    Testing IDistributedCache.

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -2,8 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+app.MapEndpoint<Platform.SumEndpoint>("/sum/{count:int=1000000000}");
+
 app.MapGet("/", async context => {
     await context.Response.WriteAsync("Hello World!");
 });
 
 app.Run();
+    
