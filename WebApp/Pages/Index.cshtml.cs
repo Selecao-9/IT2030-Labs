5a6fb4c7d2fb7da22aca946ba7c36af270fafc29
Muso <anon8434337@PM.me>
2025-07-13 12:37:35 -0400

    Testing the code behind class file.

--- WebApp/Pages/Index.cshtml.cs
+++ WebApp/Pages/Index.cshtml.cs
@@ -1,12 +1,24 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private DataContext context;
+
+        public Product? Product { get; set; }
+
+        public IndexModel(DataContext ctx)
         {
+            context = ctx;
+        }
+
+        public async Task<IActionResult> OnGetAsync(long id = 1)
+        {
+            Product = await context.Products.FindAsync(id);
+            return Page();
         }
     }
 }
