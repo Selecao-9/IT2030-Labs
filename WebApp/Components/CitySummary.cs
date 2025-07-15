06a6cb7fe93ea46a9d8eb3ec4c4abc3d24141cf0
Muso <anon8434337@PM.me>
2025-07-15 14:14:18 -0400

    Testing html content view component result.

--- WebApp/Components/CitySummary.cs
+++ WebApp/Components/CitySummary.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Html;
 
 namespace WebApp.Components
 {
@@ -15,11 +17,8 @@ namespace WebApp.Components
 
         public IViewComponentResult Invoke()
         {
-            return View(new CityViewModel
-            {
-                Cities = data.Cities.Count(),
-                Population = data.Cities.Sum(c => c.Population)
-            });
+            return new HtmlContentViewComponentResult(
+                new HtmlString("This is a <h3><i>string</i></h3>"));
         }
     }
 }
