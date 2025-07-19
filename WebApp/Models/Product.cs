c08868bf0a6df52b9442d0b551fc1c22a571146c
Muso <anon8434337@PM.me>
2025-07-19 17:57:03 -0400

    Preparing to test remote validation.

--- WebApp/Models/Product.cs
+++ WebApp/Models/Product.cs
@@ -2,13 +2,17 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using WebApp.Validation;
 
 namespace WebApp.Models
 {
+
+    [PhraseAndPrice(Phrase = "Small", Price = "100")]
     public class Product
     {
 
         public long ProductId { get; set; }
+
         [Required(ErrorMessage = "Please enter a name")]
         public required string Name { get; set; }
 
@@ -16,9 +20,13 @@ namespace WebApp.Models
         [Column(TypeName = "decimal(8, 2)")]
         public decimal Price { get; set; }
 
+        [PrimaryKey(ContextType = typeof(DataContext),
+            DataType = typeof(Category))]
         public long CategoryId { get; set; }
         public Category? Category { get; set; }
 
+        [PrimaryKey(ContextType = typeof(DataContext),
+            DataType = typeof(Supplier))]
         public long SupplierId { get; set; }
 
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
