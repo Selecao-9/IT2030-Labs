80d3011023e3c9909fabeb536f8863b0456ee83a
Muso <anon8434337@PM.me>
2025-07-19 14:50:58 -0400

    Testing form results.

--- WebApp/Controllers/FormController.cs
+++ WebApp/Controllers/FormController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using System.Text.Json;
 
 namespace WebApp.Controllers
 {
@@ -17,41 +15,27 @@ namespace WebApp.Controllers
             context = dbContext;
         }
 
-        public async Task<IActionResult> Index([FromQuery] long? id)
+        public async Task<IActionResult> Index(long? id)
         {
-            ViewBag.Categories = new SelectList(context.Categories,
-                "CategoryId", "Name");
             return View("Form", await context.Products
-                .Include(p => p.Category)
-                .Include(p => p.Supplier)
-                .FirstOrDefaultAsync(p => id == null || p.ProductId == id)
-            ?? new() { Name = string.Empty });
+                .OrderBy(p => p.ProductId)
+                .FirstOrDefaultAsync(p => id == null
+                    || p.ProductId == id));
         }
 
         [HttpPost]
-        public IActionResult SubmitForm([Bind("Name", "Category")]
-                Product product)
+        public IActionResult SubmitForm(Product product)
         {
             TempData["name"] = product.Name;
             TempData["price"] = product.Price.ToString();
-            TempData["category name"] = product.Category?.Name;
+            TempData["categoryId"] = product.CategoryId.ToString();
+            TempData["supplierId"] = product.SupplierId.ToString();
             return RedirectToAction(nameof(Results));
         }
 
         public IActionResult Results()
         {
-            return View();
-        }
-        public string Header([FromHeader(Name = "Accept-Language")]
-                string accept)
-        {
-            return $"Header: {accept}";
-        }
-        [HttpPost]
-        [IgnoreAntiforgeryToken]
-        public Product Body([FromBody] Product model)
-        {
-            return model;
+            return View(TempData);
         }
     }
 }
