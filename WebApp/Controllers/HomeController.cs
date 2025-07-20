4d851d3a50dd3ae1f30bb11dafa7c89b933e4162
Muso <anon8434337@PM.me>
2025-07-20 11:36:34 -0400

    Testing the filter applied to the entire class.

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -2,18 +2,16 @@
 
 namespace WebApp.Controllers
 {
-
+    [RequireHttps]
     public class HomeController : Controller
     {
 
-        [RequireHttps]
         public IActionResult Index()
         {
             return View("Message",
                 "This is the Index action on the Home controller");
         }
 
-        [RequireHttps]
         public IActionResult Secure()
         {
             return View("Message",
