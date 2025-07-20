6b0cfccf4c6f280f5d4c15dad0114fb952aa7ae1
Muso <anon8434337@PM.me>
2025-07-20 11:28:16 -0400

    Testing the IsHttps method.

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            if (Request.IsHttps)
+            {
+                return View("Message",
+                    "This is the Index action on the Home controller");
+            }
+            else
+            {
+                return new StatusCodeResult(
+                    StatusCodes.Status403Forbidden);
+            }
+        }
+    }
+}
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
-using Microsoft.AspNetCore.Antiforgery;
-using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,38 +10,11 @@ builder.Services.AddDbContext<DataContext>(opts => {
 });
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
-builder.Services.AddSingleton<CitiesData>();
-builder.Services.Configure<AntiforgeryOptions>(opts => {
-    opts.HeaderName = "X-XSRF-TOKEN";
-});
-
-builder.Services.Configure<MvcOptions>(opts =>
-    opts.ModelBindingMessageProvider
-    .SetValueMustNotBeNullAccessor(value => "Please enter a value"));
 
 var app = builder.Build();
 
 app.UseStaticFiles();
-
-IAntiforgery antiforgery
-    = app.Services.GetRequiredService<IAntiforgery>();
-app.Use(async (context, next) => {
-    if (!context.Request.Path.StartsWithSegments("/api"))
-    {
-        string? token =
-            antiforgery.GetAndStoreTokens(context).RequestToken;
-        if (token != null)
-        {
-            context.Response.Cookies.Append("XSRF-TOKEN",
-               token,
-               new CookieOptions { HttpOnly = false });
-        }
-    }
-    await next();
-});
-
-app.MapControllerRoute("forms",
-    "controllers/{controller=Home}/{action=Index}/{id?}");
+app.MapDefaultControllerRoute();
 app.MapRazorPages();
 
 var context = app.Services.CreateScope().ServiceProvider
