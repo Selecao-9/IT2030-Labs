d009e751f3fd110101bc36f0186b7cb42d4623af
Muso <anon8434337@PM.me>
2025-07-20 14:50:05 -0400

    Testing multiple instances of a filter on the same class.

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApp.Filters;
-using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace WebApp.Controllers
 {
 
     [HttpsOnly]
     [ResultDiagnostics]
+    [GuidResponse]
+    [GuidResponse]
     public class HomeController : Controller
     {
 
@@ -14,44 +15,6 @@ namespace WebApp.Controllers
         {
             return View("Message",
                 "This is the Index action on the Home controller");
-        }
-
-        public IActionResult Secure()
-        {
-            return View("Message",
-                "This is the Secure action on the Home controller");
-        }
-
-        //[ChangeArg]
-        public IActionResult Messages(string message1,
-                string message2 = "None")
-        {
-            return View("Message", $"{message1}, {message2}");
-        }
-
-        public override void OnActionExecuting(
-                ActionExecutingContext context)
-        {
-            if (context.ActionArguments.ContainsKey("message1"))
-            {
-                context.ActionArguments["message1"] = "New message";
-            }
-        }
-        [RangeException]
-        public ViewResult GenerateException(int? id)
-        {
-            if (id == null)
-            {
-                throw new ArgumentNullException(nameof(id));
-            }
-            else if (id > 10)
-            {
-                throw new ArgumentOutOfRangeException(nameof(id));
-            }
-            else
-            {
-                return View("Message", $"The value is {id}");
-            }
         }
     }
 }
